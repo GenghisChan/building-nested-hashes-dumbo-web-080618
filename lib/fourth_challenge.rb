6ca8b68ebbd5c055de5154abaf8a90276af80fa5
Christofer Chan <devchumbing@gmail.com>
2018-08-03 01:43:28 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -19,7 +19,7 @@ def fourth_challenge
 }
 
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:montague][:heroine][:status] = "dead"
+epic_tragedy[montague:][hero:][status:] = "dead"
+epic_tragedy[montague:][heroine:][status:] = "dead"
 
 end
